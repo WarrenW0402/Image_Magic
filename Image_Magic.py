3ae9b7c268c2241fa368d631a2005c3515a60fb5
WarrenW0402 <warrenwu0402@gmail.com>
2021-10-08 12:45:22 -0700

    Print all the rgb values of the image

--- Image_Magic.py
+++ Image_Magic.py
@@ -11,3 +11,21 @@ a_pixel = image.getpixel((0, 0))  # grab pixel (0, 0) top-left
 
 print(a_pixel)
 
+# Iterate over EVERY PIXEL
+# Get dimensions (size) of the image
+image_width = image.width
+image_height = image.height
+
+# Top to bottom
+for y in range(image_height):
+    # Left to right
+    for x in range(image_width):
+        # Grab pixel information for THIS pixel
+        pixel = image.getpixel((x, y))
+
+        print(f"\nPixel Location: {x}, {y}")
+        # Print pixel values
+        print(f"red: {pixel[0]}")
+        print(f"green: {pixel[1]}")
+        print(f"blue: {pixel[2]}")
+
